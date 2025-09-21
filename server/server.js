d13c6266441af41d88ca31b9b5eae5fe3f87f4ac
vedastra <weblog.arham129@gmail.com>
2025-09-21 21:14:00 +0530

    refactor: correct export statement for app in server.js

--- server/server.js
+++ server/server.js
@@ -25,4 +25,4 @@ if (process.env.NODE_ENV !== 'production' ) {
  const PORT = process.env.PORT || 5000 
 app.listen(PORT, () => { console.log('Server is running on PORT:' + PORT) })}
 
-export default server
+export default app
